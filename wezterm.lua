c9b6d8f59562a5202c235b19e9031db58d1f3b55
Alejandro Rodriguez <alejandro.rodriguez@AT-NB-578.local>
2024-09-03 13:00:18 +0200

    basic config

---
+++ wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require('wezterm')
+
+-- wezterm.log_info("THIS IS A LOG!!")
+-- WEZTERM_LOG=info wezterm
+
+local config = {
+  -- appearence
+  color_scheme = 'tokyonight_storm',
+  -- color_scheme = "tokyonight_night",
+
+  -- enable_tab_bar = false,
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+
+  font = wezterm.font('Hack Nerd Font'),
+
+  inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.8,
+    -- opacity = 0.0,
+  },
+  -- inactive_pane_opacity = 0.5,
+  window_background_opacity = 1,
+
+  window_decorations = 'RESIZE',
+
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
+}
+
+return config
